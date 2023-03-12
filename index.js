98cf93fbf26b1b891ec69798488a15332f7f3c1e
Daniel Karanja <sirkaranja12@gmail.com>
2023-03-12 23:47:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
+
+input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+  const input = document.getElementById("button")
+  input.addEventListener('click', function(event) {
+      return "clicked"
+    });
 }
